a4e9b76c2e5a8e136fa5db07ce13f312d673225c
rubbycool <ogaduighodalo@gmail.com>
2022-06-30 14:43:52 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,17 +1,14 @@
-	@@ -1,15 +1,14 @@
-	#include <stdio.h>
+#include <stdio.h>
 
-	/**
-	 * main - Entry point
-	 *
-	 * Return: Always 1 (Success)
-	 */
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 
-	int write(int filedes, const char *buf, unsigned int nbyte);
-	int main(void)
-
-	{	
-		write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
-
-		return (1);
-	}
+int write(int filedes, const char *buf, unsigned int nbyte);
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+	return (1);
+}
